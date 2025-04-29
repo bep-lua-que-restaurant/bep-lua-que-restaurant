4d3c055d3efb3f5280267193961d44caa5cb2b34
vhung02 <viethung021002@gmail.com>
2025-04-29 16:07:26 +0700

    fix bug ngày 29/4

--- bep-lua-que/resources/views/admin/monan/list.blade.php
+++ bep-lua-que/resources/views/admin/monan/list.blade.php
@@ -86,7 +86,7 @@
                 serverSide: true,
                 ajax: '{{ $route }}',
                 columns: [{
-                    data: 'DT_RowIndex', // ✅ Số thứ tự
+                        data: 'DT_RowIndex', // ✅ Số thứ tự
                         name: 'DT_RowIndex',
                         orderable: false,
                         searchable: false
@@ -115,7 +115,23 @@
                     }
                 ],
                 language: {
-                    url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/vi.json'
+                    processing: "Đang xử lý...",
+                    lengthMenu: "Hiển thị _MENU_ dòng mỗi trang",
+                    zeroRecords: "Không tìm thấy dữ liệu phù hợp",
+                    info: "Hiển thị _START_ đến _END_ của _TOTAL_ dòng",
+                    infoEmpty: "Hiển thị 0 đến 0 của 0 dòng",
+                    infoFiltered: "(lọc từ _MAX_ tổng số dòng)",
+                    search: "Tìm kiếm:",
+                    paginate: {
+                        first: "Đầu",
+                        previous: "Trước",
+                        next: "Tiếp",
+                        last: "Cuối"
+                    },
+                    aria: {
+                        sortAscending: ": Sắp xếp tăng dần",
+                        sortDescending: ": Sắp xếp giảm dần"
+                    }
                 },
                 pagingType: 'full_numbers',
                 renderer: 'bootstrap',
